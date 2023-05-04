35b55c919457830c108e6ad03379ddf43875f90c
harrychangjr <71932325+harrychangjr@users.noreply.github.com>
2023-05-05 00:17:56 +0800

    hashtag dboards

--- pages/content.py
+++ pages/content.py
@@ -12,6 +12,7 @@ from st_aggrid import AgGrid
 import io
 import re
 import emoji
+from collections import Counter
 from st_pages import Page, show_pages, add_page_title
 
 # Set page title
@@ -70,6 +71,8 @@ if uploaded_files:
                 data['Hashtags'] = data['Video title'].apply(extract_hashtags)
                 #data['Emojis'] = data['Video title'].apply(extract_emojis)
                 data['Cleaned_title'] = data['Video title'].apply(clean_title)
+                # Add a new column to store the hashtag count
+                data['Hashtag_count'] = data['Hashtags'].apply(len)
                 st.write(data)
                 options = ["Total views", "Total shares", "Total likes", "Total comments", "Hashtags"]
                 selected_feature = st.selectbox(label="Select feature", options=options, index=0)
@@ -93,8 +96,74 @@ if uploaded_files:
                     fig = px.bar(data, x='Total comments', y='Cleaned_title', title='Comments of trending videos for the week',
                             color_discrete_sequence=px.colors.qualitative.Vivid, hover_data={'Total comments': ':.2f'})
                     st.plotly_chart(fig)
+                elif selected_feature == "Hashtags":
+                    # Count the occurrences of each hashtag
+                    hashtag_counts = Counter(hashtag for hashtags in data['Hashtags'] for hashtag in hashtags)
+
+                    # Get the top N most common hashtags
+                    N = 10
+                    top_hashtags = hashtag_counts.most_common(N)
+
+                    # Display the top hashtags
+                    print(f"Top {N} hashtags:")
+                    for hashtag, count in top_hashtags:
+                        print(f"{hashtag}: {count}")
+
+                    # Visualize the results with a Plotly bar chart
+                    fig = go.Figure(go.Bar(
+                        x=[t[0] for t in top_hashtags],
+                        y=[t[1] for t in top_hashtags],
+                        text=[t[1] for t in top_hashtags],
+                        textposition='auto',
+                        marker_color='rgba(58, 71, 80, 0.6)',
+                        opacity=0.8
+                    ))
+
+                    fig.update_layout(
+                        title=f'Top {N} Hashtags',
+                        xaxis_title='Hashtags',
+                        yaxis_title='Count',
+                        xaxis_tickangle=-45
+                    )
+
+                    st.plotly_chart(fig)
+
+                    tab1, tab2, tab3, tab4 = st.tabs(["vs Views", "vs Shares", "vs Likes", "vs Comments"])
+                    with tab1:
+                        fig = px.scatter(data, x='Hashtag_count', y='Total views', hover_data=['Cleaned_title'])
+                        fig.update_layout(
+                            title='Hashtag Count vs. Views',
+                            xaxis_title='Hashtag Count',
+                            yaxis_title='Views'
+                        )
+                        st.plotly_chart(fig)
+                    with tab2:
+                        fig = px.scatter(data, x='Hashtag_count', y='Total shares\xa0', hover_data=['Cleaned_title'])
+                        fig.update_layout(
+                            title='Hashtag Count vs. Shares',
+                            xaxis_title='Hashtag Count',
+                            yaxis_title='Shares'
+                        )
+                        st.plotly_chart(fig)
+                    with tab3:
+                        fig = px.scatter(data, x='Hashtag_count', y='Total likes', hover_data=['Cleaned_title'])
+                        fig.update_layout(
+                            title='Hashtag Count vs. Likes',
+                            xaxis_title='Hashtag Count',
+                            yaxis_title='Likes'
+                        )
+                        st.plotly_chart(fig)
+                    with tab4:
+                        fig = px.scatter(data, x='Hashtag_count', y='Total comments', hover_data=['Cleaned_title'])
+                        fig.update_layout(
+                            title='Hashtag Count vs. Comments',
+                            xaxis_title='Hashtag Count',
+                            yaxis_title='Comments'
+                        )
+                        st.plotly_chart(fig)
+
 
         with tab2:
             if 'video_view_within_days' not in data.columns: #Video Posts
-                st.write("No")
+                pass#st.write("No")
         
